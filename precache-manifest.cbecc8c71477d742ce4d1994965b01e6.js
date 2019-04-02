20367fbd042c2950c41dfbaa1762e4f3833a151a
H4kan <szymon.sieradzki184@wp.pl>
2019-04-02 16:43:15 +0200

    Updates

---
+++ precache-manifest.cbecc8c71477d742ce4d1994965b01e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d9660ac7231c75aaa09c62dc3f7f705",
+    "url": "/Enigma/static/media/background.5d9660ac.jpg"
+  },
+  {
+    "revision": "1df7e9b79f38e92e34f3",
+    "url": "/Enigma/static/js/runtime~main.ba1348bb.js"
+  },
+  {
+    "revision": "3b988a4ad0b613367c2a",
+    "url": "/Enigma/static/js/main.b35add89.chunk.js"
+  },
+  {
+    "revision": "dbb4ac401103bee4492d",
+    "url": "/Enigma/static/js/2.4a38c754.chunk.js"
+  },
+  {
+    "revision": "3b988a4ad0b613367c2a",
+    "url": "/Enigma/static/css/main.6964f630.chunk.css"
+  },
+  {
+    "revision": "22367fc518179cd597764bfdfd2e91e3",
+    "url": "/Enigma/index.html"
+  }
+];
